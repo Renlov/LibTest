9873e9507409c59a33ee9e8f8c6d858c23227b99
renlov <a.pimenov95@mail.ru>
2022-02-10 15:56:59 +0300

    added

--- app/src/main/java/com/test1/testlib/MainActivity.kt
+++ app/src/main/java/com/test1/testlib/MainActivity.kt
@@ -2,13 +2,13 @@ package com.test1.testlib
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.test1.toasttest.tech
+import com.test1.toasttest.toast
 
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        tech("sapport")
+        toast("aaaa")
     }
 }
--- app/toasttest/src/main/java/com/test1/toasttest/Tech.kt
+++ app/toasttest/src/main/java/com/test1/toasttest/Tech.kt
@@ -6,9 +6,9 @@ import androidx.fragment.app.Fragment
 
 
 fun <T : Activity> T.toast(message: String) {
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    Toast.makeText(this, message  + "version 2", Toast.LENGTH_SHORT).show()
 }
 
 fun <T : Fragment> T.toast(message: String) {
-    Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    Toast.makeText(requireContext(),message  + "version 2", Toast.LENGTH_SHORT).show()
 }
